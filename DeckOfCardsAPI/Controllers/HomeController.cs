0413e5419ce330b2b55ae43448ca306b2f1f438d
Jesse Todd <jessetodd@rocketmortgage.com>
2022-10-17 17:25:15 -0400

    updated

--- DeckOfCardsAPI/Controllers/HomeController.cs
+++ DeckOfCardsAPI/Controllers/HomeController.cs
@@ -15,16 +15,21 @@ namespace DeckOfCardsAPI.Controllers
 
         async public Task<IActionResult> Index()
         {
-            HttpClient web = new HttpClient();
-            web.BaseAddress = new Uri("https://www.deckofcardsapi.com/api/");
-            var connection = await web.GetAsync("deck/new/shuffle/?deck_count=1");
-            CardResponse deck = await connection.Content.ReadAsAsync<CardResponse>();
+            //HttpClient web = new HttpClient();
+            //web.BaseAddress = new Uri("https://www.deckofcardsapi.com/api/");
+            //var connection = await web.GetAsync("deck/new/shuffle/?deck_count=1");
+            //CardResponse deck = await connection.Content.ReadAsAsync<CardResponse>();
 
-            string deckid = deck.deck_id;
-            var connectionDraw = await web.GetAsync($"deck/{deckid}/draw/?count=5");
-            deck = await connectionDraw.Content.ReadAsAsync<CardResponse>();
+            string deck_id = await CardAPI.GetNewDeck();
+            CardResponse resp = await CardAPI.GetCards(deck_id, 5);
 
-            return View(deck.cards);
+            return View(resp);
+        }
+
+        async public Task<IActionResult> DrawFive(string deck_id)
+        {
+            CardResponse resp = await CardAPI.GetCards(deck_id, 5);
+            return View("index", resp);
         }
 
         //async public Task<IActionResult> Draw(string deckid)
--- DeckOfCardsAPI/Models/CardResponse.cs
+++ DeckOfCardsAPI/Models/CardResponse.cs
@@ -17,4 +17,42 @@
         public string value { get; set; }
         public string suit { get; set; }
     }
+
+	public class CardAPI
+	{
+		//    GetNewDeck()
+		//    GetCards(deck_id, count)
+
+		public static HttpClient _web = null;
+
+		public static HttpClient GetHttpClient()
+		{
+			if (_web == null)
+			{
+				_web = new HttpClient();
+				_web.BaseAddress = new Uri("https://www.deckofcardsapi.com/api/deck/");
+
+			}
+			return _web;
+		}
+
+		async public static Task<string> GetNewDeck()
+		{
+			HttpClient web = GetHttpClient();
+
+			var connection = await web.GetAsync("new/shuffle/?deck_count=1");
+			CardResponse resp = await connection.Content.ReadAsAsync<CardResponse>();
+
+			return resp.deck_id;
+		}
+
+		async public static Task<CardResponse> GetCards(string deck_id, int count)
+		{
+			HttpClient web = GetHttpClient();
+			var connection = await web.GetAsync($"{deck_id}/draw/?count={count}");
+			CardResponse resp = await connection.Content.ReadAsAsync<CardResponse>();
+			return resp;
+		}
+	}
+
 }
